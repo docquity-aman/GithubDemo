c7966f40ae174bba6fd2540c5a8dddf7a3bfca4d
Aman <aman.verma@docquity.com>
2023-02-08 11:20:34 +0530

    Initial Commit

---
+++ GithubDemo/GithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoApp.swift
+//  GithubDemo
+//
+//  Created by Aman Verma on 08/02/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
